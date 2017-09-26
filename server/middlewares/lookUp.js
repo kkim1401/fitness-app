d99932b0bcd06d13feaefab8ac6f97698c75a501
Kevin Kim <ykk3416@gmail.com>
2017-09-26 02:26:08 -0400

    changed error-handling for lookUp middleware, since queries don't trigger callback's error

--- server/middlewares/lookUp.js
+++ server/middlewares/lookUp.js
@@ -1,9 +1,13 @@
 const lookUp = Model => (req, res, next) => {
     Model.findById(req.params.id, (err, doc) => {
         if (err) {
-            err.status = 404;
             return next(err);
         }
+
+        if (!doc) {
+            return next(new Error("Query returned nothing"));
+        }
+
         req.doc = doc;
         next();
     });
